1cc516b43027854b1428f19aa6fab47dfa12458f
Doniel Acosta <donielacosta1995@gmail.com>
2022-08-25 17:25:51 -0400

    modificando rutas y refactorizando el testing

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+}); 
 
 Route::get('about', function () {
-  return 'Pruebas ';
+    return 'hola soy about';
 });
+
+Route::view('profile', 'profile');
+Route::post('profile', [App\Http\Controllers\ProfileController::class, 'upload']);
